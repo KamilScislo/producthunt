7771c6b4f82d6a26b4333ed053d6f86e59f1471d
KamilScislo <kamil.m.scislo@gmail.com>
2019-08-15 21:47:01 +0200

    add product model

--- producthunt-project/products/models.py
+++ producthunt-project/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=100)
+	url =  models.TextField(max_length=100)
+	pub_date = models.DateTimeField()
+	votes_total = models.IntegerField(default=1)
+	image = models.ImageField(upload_to='images/')
+	icon = models.ImageField(upload_to='images/', height_field=32, width_field=32)
+	body = models.TextField(default=' ')
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.title
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y') 

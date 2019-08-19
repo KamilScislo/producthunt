9e42e2a2d7dec91f1c9bd8ec8d1a59d883fb265b
KamilScislo <kamil.m.scislo@gmail.com>
2019-08-19 21:29:45 +0200

    add detail page and polish

--- producthunt-project/products/models.py
+++ producthunt-project/products/models.py
@@ -8,12 +8,19 @@ class Product(models.Model):
 	pub_date = models.DateTimeField()
 	votes_total = models.IntegerField(default=1)
 	image = models.ImageField(upload_to='images/')
-	icon = models.ImageField(upload_to='images/', height_field=32, width_field=32)
+	icon = models.ImageField(upload_to='images/')
 	body = models.TextField(default=' ')
 	hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 	
 	def __str__(self):
 		return self.title
 
+	def summary(self):
+		return self.body[:200] + '...'
+
+
 	def pub_date_pretty(self):
 		return self.pub_date.strftime('%b %e %Y') 
+
+
+
